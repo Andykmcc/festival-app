af00bd55bea92e84283b2b404d45e23027fdd43d
andykmccoy <andy.k.mccoy@gmail.com>
2015-12-13 22:45:45 -0800

    basic gulp build process in place

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
+var config = require('./config/dev');
 var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
+plugins.browserify = require('browserify');
+plugins.source = require('vinyl-source-stream');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('connect', function() {
+  plugins.connect.server({
+    root: config.build,
+    livereload: true
+  });
 });
+
+gulp.task('html', function() {
+  return gulp.src(config.src + '/*.html')
+    .pipe(gulp.dest(config.build))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('browserify', function() {
+  // Grabs the app.js file
+  return plugins.browserify(config.src + '/app.js')
+    // bundles it and creates a file called main.js
+    .bundle()
+    .pipe(plugins.source('main.js'))
+    // saves it the src/ directory
+    .pipe(gulp.dest(config.build + '/js/'))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('src/**/*.js', ['browserify']);
+  // Watches for changes in style.sass and runs the sass task
+  // gulp.watch('src/scss/style.sass', ['sass'])
+})
+
+gulp.task('default', ['connect', 'watch']);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+var angular = require('angular');
+
+angular.module('am.fest', [])
+.controller('MainController', function($scope) {
+  $scope.message = 'Angular Works!!!!';
+});
